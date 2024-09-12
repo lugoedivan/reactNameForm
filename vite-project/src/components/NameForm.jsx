01436edb438b220d9bfa396c974d68896ae9822f
Edivan Lugo <lugo.edivan@gmail.com>
2024-09-12 11:07:20 -0400

    added comments

--- vite-project/src/components/NameForm.jsx
+++ vite-project/src/components/NameForm.jsx
@@ -1,8 +1,10 @@
 import { useState } from "react";
 
 function NameForm() {
+  //Here we create the variable we want to use and we asign a usestate to be able to update its value
   const [name, setName] = useState("");
 
+  //Here we create a function to update the variable value from the input
   const handleInputChange = (event) => {
     setName(event.target.value);
   };
@@ -10,7 +12,9 @@ function NameForm() {
   return (
     <div>
       <h2>Enter your name:</h2>
+      {/* This allows the input to be  updated in real time */}
       <input type="text" value={name} onChange={handleInputChange} />
+      {/* Here we display the input */}
       <p>Hi, {name}!</p>
     </div>
   );
